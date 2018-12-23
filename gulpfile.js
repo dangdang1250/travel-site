84b65bedbd4a515b7cbbb9e1609c12be5f8cda35
jicheng <chengjixuan@hotmail.com>
2018-12-23 11:05:27 -0500

    install a lot of gulp package down grade gulp's version from 4.0.0 to 3.9.1 because 4.0 didn't know start function and I don't know how to fix it yet. So gult wathc is working now This is the end of session 4
    On branch WebSession4
    modified:   .gitignore
    modified:   app/assets/styles/style.css
    modified:   gulpfile.js
    modified:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+var gulp     = require('gulp'),
+watch        = require('gulp-watch'),
+postcss      = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars      = require('postcss-simple-vars'),
+nested       = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -10,5 +14,18 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-  console.log("Imagine Sass or PostCSS tasks running here.");
+	return gulp.src('./app/assets/styles/style.css')
+		.pipe(postcss([nested,cssvars,autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+	watch('./app/index.html',function(){
+		gulp.start('html');
+	});
+
+  	watch('app/assets/styles/*.css',function(){
+  		gulp.start('styles');
+	}); 
+  
 });
