587c2b775cceaa81bc458a766594c1b29aed5ac2
jicheng <chengjixuan@hotmail.com>
2018-12-23 09:59:38 -0500

    add gulpfule.js to do task add gulp plugin gulp-watch, but didn't work error is about gulp.start is not a function. might because version chanes
    On branch WebSession4
    new file:   app/assets/styles/style.css
    new file:   gulpfile.js
    modified:   package.json

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
+  console.log("Imagine Sass or PostCSS tasks running here.");
+});
