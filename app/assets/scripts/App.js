ab44fccb870353c120332f79ea289f97c017812d
jicheng <chengjixuan@hotmail.com>
2018-12-24 18:33:52 -0500

    Integrating "webpack" into gulp create watch tasks and reload On branch WebSession14 modified:   app/assets/scripts/App.js modified:   app/temp/scripts/App.js new file:   gulp/tasks/scripts.js modified:   gulp/tasks/watch.js modified:   gulpfile.js modified:   package.json

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,14 @@
 //Need to install webpack 
 
 // import jquery package
+
 var $ = require('jquery');
 var Person = require('./modules/Person'); //you don't need .js 
 
+//alert("It is refresh by it self now.");
 var john = new Person("John","blue");
 john.greet();
 var jane = new Person("Jane","green");
 jane.greet();
 
-$("h1").remove();
+//$("h1").remove();
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("//Need to install webpack \n\n// import jquery package\nvar $ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\nvar Person = __webpack_require__(/*! ./modules/Person */ \"./app/assets/scripts/modules/Person.js\"); //you don't need .js \n\nvar john = new Person(\"John\",\"blue\");\njohn.greet();\nvar jane = new Person(\"Jane\",\"green\");\njane.greet();\n\n$(\"h1\").remove();\n\n//# sourceURL=webpack:///./app/assets/scripts/App.js?");
+eval("//Need to install webpack \n\n// import jquery package\n\nvar $ = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\nvar Person = __webpack_require__(/*! ./modules/Person */ \"./app/assets/scripts/modules/Person.js\"); //you don't need .js \n\n//alert(\"It is refresh by it self now.\");\nvar john = new Person(\"John\",\"blue\");\njohn.greet();\nvar jane = new Person(\"Jane\",\"green\");\njane.greet();\n\n//$(\"h1\").remove();\n\n//# sourceURL=webpack:///./app/assets/scripts/App.js?");
 
 /***/ }),
 
