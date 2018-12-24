15f341a256b66011d788f95f490ea3164202c1da
jicheng <chengjixuan@hotmail.com>
2018-12-24 12:09:00 -0500

    fix types add new gulp task
    On branch WebSession12
    modified:   app/assets/styles/modules/_page-section.css
    modified:   app/assets/styles/modules/_sprite.css
    modified:   app/index.html
    modified:   app/temp/sprite/css/sprite.css
    modified:   app/temp/styles/styles.css
    modified:   gulp/tasks/sprites.js
    modified:   gulp/templates/sprite.css

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,6 +5,7 @@ rename       = require('gulp-rename');
 var config = {
 	mode: {
 		css:{
+			sprite:'sprite.svg',
 			render:{
 				css:{
 					template: './gulp/templates/sprite.css'
@@ -17,10 +18,17 @@ gulp.task('createSprite',function(){
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 	.pipe(svgSprite(config))
 	.pipe(gulp.dest('./app/temp/sprite/'));
-})
+});
 
-gulp.task('copySpriteCSS',function(){
+gulp.task('copySpriteGraphic',['createSprite'],function(){
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+	.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS',['createSprite'],function(){
 	return gulp.src('./app/temp/sprite/css/*.css')
 	.pipe(rename('_sprite.css'))
 	.pipe(gulp.dest('./app/assets/styles/modules'));
-})
+});
+
+gulp.task('icons',['createSprite','copySpriteGraphic','copySpriteCSS']);
