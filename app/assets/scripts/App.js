82386107b3c4bbbc9cc43362f3a6e8951c52b05c
jicheng <chengjixuan@hotmail.com>
2018-12-24 18:20:05 -0500

    Learn how to use jquery. On branch WebSession14 modified:   app/assets/scripts/App.js modified:   app/temp/scripts/App.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
 //Need to install webpack 
+
+// import jquery package
+var $ = require('jquery');
 var Person = require('./modules/Person'); //you don't need .js 
 
 var john = new Person("John","blue");
 john.greet();
 var jane = new Person("Jane","green");
 jane.greet();
+
+$("h1").remove();
