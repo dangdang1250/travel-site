d107bb4fbdb4bb3518570489a76f11e9f53d21a8
jicheng <chengjixuan@hotmail.com>
2018-12-25 09:54:19 -0500

    Fix a typo

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,31 +1,30 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
-class RevealOnScroll{
-	constructor(){
-		this.itemsToReveal = $(".feature-item");
-		this.hideInitiall();
-		this.createWayPoints();
-	}
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $(".feature-item");
+    this.hideInitially();
+    this.createWaypoints();
+  }
 
-	hideInitiall(){
-		this.itemsToReveal.addClass("reveal-item");
-	}
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
 
-	createWayPoints(){
-		this.itemsToReveal.each(function(){
-			//alert("testing");
-			var currentItem = this;
-			new Waypoint({
-				elements: currentItem,
-				handler : function(){
-					$(currentItem).addClass("reveal-item--is-visible");
-				},
-				offset:"85%"
-			});
-		});
-	}
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: "85%"
+      });
+    });
+  }
 }
-//use npm install waypoints --save
 
 export default RevealOnScroll;
+//use npm install waypoints --save
