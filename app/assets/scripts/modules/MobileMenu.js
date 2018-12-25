2826587bd70cdb8e67802755cdc7cd9825654f55
jicheng <chengjixuan@hotmail.com>
2018-12-24 19:45:42 -0500

    Add menu class use java script dynamic add elements now On branch WebSession15 modified:   app/assets/scripts/modules/MobileMenu.js modified:   app/assets/styles/modules/_site-header.css modified:   app/temp/scripts/App.js modified:   app/temp/styles/styles.css

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,10 +2,22 @@ import $ from 'jquery';
 
 class MobileMenu{
 	constructor(){
-		$('.site-header__menu-icon').click(function(){
-			console.log("The top right icon was clicked");
-		});
-		//alert("Testing from MobileMenu");
+		this.menuIcon = $(".site-header__menu-icon");
+		this.menuContent = $(".site-header__menu-content");
+		this.events();//must add
+	}
+	//watch for events
+	events(){
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+		console.log(this);
+	}
+
+	toggleTheMenu(){
+		//console.log("This icon was clicked!!!");
+		//console.log("here"+this); this is not working here now
+		//alert(this); //hello
+		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+
 	}
 }
 
