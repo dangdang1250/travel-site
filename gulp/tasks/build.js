227635f3245cdce9e150247ba1541f943202e13c
jicheng <chengjixuan@hotmail.com>
2018-12-25 18:50:09 -0500

    change dist name to docschange dist name to docschange dist name to docschange dist name to docschange dist name to docschange dist name to docschange dist name to docschange dist name to docs

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,13 +11,13 @@ gulp.task('previewDist', function(){
 	browserSync.init({
 		notify:false,
 		server:{
-			baseDir:"dist"
+			baseDir:"docs"
 		}
 	});
 });
 
-gulp.task('deleteDistFolder',function(){
-	return del('./dist');
+gulp.task('deleteDistFolder', ['icons'],function(){
+	return del('./docs');
 });
 
 gulp.task('copyGeneralFiles',['deleteDistFolder'],function(){
@@ -30,10 +30,10 @@ gulp.task('copyGeneralFiles',['deleteDistFolder'],function(){
 		'!./app/temp/**'
 	];
 	return gulp.src(pathsToCopy)
-	.pipe(gulp.dest('./dist'));
+	.pipe(gulp.dest('./docs'));
 });
 
-gulp.task('optimizeImages',['deleteDistFolder','icons'],function(){
+gulp.task('optimizeImages',['deleteDistFolder'],function(){
 	return gulp.src(['./app/assets/images/**/*',
 		'!./app/assets/images/icons',
 		'!./app/assets/images/icons/**/*'])
@@ -42,10 +42,15 @@ gulp.task('optimizeImages',['deleteDistFolder','icons'],function(){
 		interlaced:  true,
 		multipass :  true
 	}))
-	.pipe(gulp.dest('./dist/assets/images'));
+	.pipe(gulp.dest('./docs/assets/images'));
 });
 
-gulp.task('usemin',['deleteDistFolder','styles','scripts'],function(){
+gulp.task('useminTrigger',['deleteDistFolder'],function()
+{
+	gulp.start('t_usemin');
+});
+
+gulp.task('t_usemin',['styles','scripts'],function(){
 	return gulp.src('./app/index.html')
 	.pipe(usemin({
 		css: [function(){return rev();},
@@ -59,7 +64,7 @@ gulp.task('usemin',['deleteDistFolder','styles','scripts'],function(){
 		}
 		]
 	}))
-	.pipe(gulp.dest("./dist"));
+	.pipe(gulp.dest("./docs"));
 });
 
-gulp.task('build',['deleteDistFolder','copyGeneralFiles','optimizeImages','usemin']);
+gulp.task('build',['deleteDistFolder','copyGeneralFiles','optimizeImages','t_usemin','useminTrigger']);
