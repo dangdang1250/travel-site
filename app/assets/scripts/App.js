9bb4e301d0b6b41612ef9761dc3959111c846d0a
jicheng <chengjixuan@hotmail.com>
2018-12-25 10:03:07 -0500

    Fix issues and improve RevealOnScroll so we can Reuse RevealOnScroll class. Very nice On branch WebSession16 modified:   app/assets/scripts/App.js modified:   app/assets/scripts/modules/RevealOnScroll.js modified:   app/temp/scripts/App.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
-var $ = require('jquery');
+//var $ = require('jquery');
 //use es6 way
 import Person         from './modules/Person';
 import MobileMenu     from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
