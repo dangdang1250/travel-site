822ebf9d6f607dd4bad1dddeb80e2f23d710bb60
jicheng <chengjixuan@hotmail.com>
2018-12-24 20:05:11 -0500

    Adjusting Mobile Menu: Learned a lot
    On branch WebSession15
    modified:   app/assets/scripts/modules/MobileMenu.js
    modified:   app/assets/styles/modules/_primary-nav.css
    modified:   app/assets/styles/modules/_site-header.css
    modified:   app/temp/scripts/App.js
    modified:   app/temp/styles/styles.css

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class MobileMenu{
 	constructor(){
+		this.siteHeader = $(".site-header");
 		this.menuIcon = $(".site-header__menu-icon");
 		this.menuContent = $(".site-header__menu-content");
 		this.events();//must add
@@ -17,6 +18,7 @@ class MobileMenu{
 		//console.log("here"+this); this is not working here now
 		//alert(this); //hello
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+		this.siteHeader.toggleClass('site-header--is-expanded');
 
 	}
 }
